6d1aa07213450701601ab6c371f57c5df2f6499e
Jason <jlynn911@gmail.com>
2015-06-25 19:58:35 -0500

    added basic ui for login and friends list

--- src/org/redmachine/XmppManager.java
+++ src/org/redmachine/XmppManager.java
@@ -1,5 +1,7 @@
 package org.redmachine;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.*;
 import java.io.*;
 
@@ -11,6 +13,8 @@ import org.jivesoftware.smack.RosterEntry;
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
 import org.jivesoftware.smack.packet.Message;
+
+import javax.swing.*;
 
 public class XmppManager implements MessageListener{
 
@@ -31,16 +35,21 @@ public class XmppManager implements MessageListener{
 		chat.sendMessage(message);
 	}
 
-	public void displayBuddyList()
-	{
-		Roster roster = connection.getRoster();
-		Collection<RosterEntry> entries = roster.getEntries();
+    public Collection<RosterEntry> buddyListCount(){
+        Roster roster = connection.getRoster();
+        Collection<RosterEntry> entries = roster.getEntries();
+        return entries;
+    }
 
-		System.out.println("\n\n" + entries.size() + " buddy(ies):");
+	public void displayBuddyList(JPanel panel)
+    {
+        Collection<RosterEntry> entries = buddyListCount();
+
 		for(RosterEntry r:entries)
 		{
-			System.out.println(r.getUser());
+            panel.add(new JLabel(r.getName()));
 		}
+
 	}
 
 	public void disconnect()
@@ -55,41 +64,100 @@ public class XmppManager implements MessageListener{
 			    System.out.println(chat.getParticipant() + " says: " + message.getBody());
 	}
 
-	public static void main(String args[]) throws XMPPException, IOException
-	{
-		// declare variables
-		XmppManager c = new XmppManager();
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String msg;
+    private static String username;
+    private static char[] password;
 
+	public static void main(String args[]) throws XMPPException, IOException {
+        // declare variables
+        XmppManager c = new XmppManager();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		// turn on the enhanced debugger
-		XMPPConnection.DEBUG_ENABLED = true;
+        // turn on the enhanced debugger
+        XMPPConnection.DEBUG_ENABLED = false;
 
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        JFrame frame = new JFrame("CCChat");
 
-		// Enter your login information here
-		c.login("redmachine", "husker");
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
-		c.displayBuddyList();
+        JLabel label = new JLabel("Login to CC Chat!");
+        JLabel userLabel = new JLabel("Username");
+        JLabel passLabel = new JLabel("Password");
+        JTextField userField = new JTextField(20);
+        JPasswordField passField = new JPasswordField(20);
+        JButton button = new JButton();
+        button.setText("Login");
 
-		System.out.println("-----");
+        panel.add(label);
+        panel.add(userLabel);
+        panel.add(userField);
+        panel.add(passLabel);
+        panel.add(passField);
+        panel.add(button);
 
-		System.out.println("Who do you want to talk to? - Type contacts full email address:");
-		String talkTo = br.readLine();
+        frame.add(panel);
 
-		System.out.println("-----");
-		System.out.println("All messages will be sent to " + talkTo);
-		System.out.println("Enter your message in the console:");
-		System.out.println("-----\n");
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
 
-		while( !(msg=br.readLine()).equals("bye"))
-		{
-			c.sendMessage(msg, talkTo);
-		}
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Username - " + userField.getText());
+                System.out.println("Password - " + passField.getPassword());
+                String pass = new String(passField.getPassword());
+                c.startXMP(c, userField.getText(), pass, frame);
+                panel.setVisible(false);
+            }
+        });
+    }
 
-		c.disconnect();
-		System.exit(0);
-	}
+    private static void startXMP(XmppManager c,String user, String pass, JFrame frame){
+        try {
+            c.login(user, pass);
+        }catch(XMPPException ex){
+            System.out.println(ex);
+        }
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.add(new JLabel("Welcome - " + user));
+        panel.add(new JLabel(" "));  //used for poor mans space
+        panel.add(new JLabel("Friends List"));
+        c.displayBuddyList(panel);
+
+        frame.add(panel);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+    }
+//
+//		// Enter your login information here
+//		c.login("redmachine", "husker");
+//
+//		c.displayBuddyList();
+//
+//		System.out.println("-----");
+//
+//		System.out.println("Who do you want to talk to? - Type contacts full email address:");
+//		String talkTo = br.readLine();
+//
+//		System.out.println("-----");
+//		System.out.println("All messages will be sent to " + talkTo);
+//		System.out.println("Enter your message in the console:");
+//		System.out.println("-----\n");
+//
+//		while( !(msg=br.readLine()).equals("bye"))
+//		{
+//			c.sendMessage(msg, talkTo);
+//		}
+//
+//		c.disconnect();
+//		System.exit(0);
 
 	class MyMessageListener implements MessageListener {
 
